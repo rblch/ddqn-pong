90a80125b53ed93a0421da62574747ee49a7669d
Raul <raulbloch@icloud.com>
2024-09-22 09:50:12 +0200

    Add .DS_Store to .gitignore

--- main.py
+++
@@ -1,8 +0,0 @@
-from trainers.trainer import Trainer
-
-def main():
-    trainer = Trainer(config=None)  # Configuration is handled internally
-    trainer.train()
-
-if __name__ == "__main__":
-    main()
